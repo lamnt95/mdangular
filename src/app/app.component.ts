2090fdc8992f331fea2c86dcdce5d2621cd5c73b
lamnt95 <tunglam20132208@gmail.com>
2022-04-08 16:34:02 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent implements OnInit {
     // this.load();
   }
   ngOnInit(): void {
-    this.load();
+    // this.load();
     setInterval(() => {
       this.isValid = false;
     }, 600000);
