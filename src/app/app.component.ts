7aa53333468d00c9235d43fb51cd7f9c87c7d6cd
lamnt95 <tunglam20132208@gmail.com>
2022-04-12 16:38:46 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -77,6 +77,9 @@ export class AppComponent implements OnInit {
     const id = _.get(this.res2, '0.i');
     const ait = await this.apiService.getOne(id);
     this.selected = ait;
+    if ((this.selected.docType = 'html')) {
+      this.selected.dateStr = this.toStrDate(this.selected.date);
+    }
     this.loading = false;
   }
 
